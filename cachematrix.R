bb5dcb6f86c5d10352ed293d13f64330dd53775b
Adam Schloss <amschloss@gmail.com>
2020-07-07 19:24:59 -0500

    Define logic for makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,18 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Establishes a cache for matrix inversion
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(newinv) inv <<- newinv
+    getinv <- function() inv
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
